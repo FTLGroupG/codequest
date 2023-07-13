bef2fb42a21f627eb672b7f951f4eb00293ad2b0
KennedyParks <kennedyjparks@gmail.com>
2023-07-13 11:51:23 -0700

    Fixed home page buttons

--- codequest-ui/src/components/Home/Home.jsx
+++ codequest-ui/src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
           <h1>Learn how to code in a fun and interactive way!</h1>
           <img src="https://www.usnews.com/object/image/00000171-9ce7-d084-affd-9def28d10000/200421-boylaptop-stock.jpg?update-time=1587475425427&size=responsive640" alt="Child sitting at a desk working on a laptop"></img>
         </div>
-        <button><Link to="/register">Sign Up</Link></button>
+        <Link to="/register"><button>Sign Up</button></Link>
 
         <div className='Card'>
           <h2>Want to try a lesson before signing up?</h2>
@@ -18,7 +18,7 @@ export default function Home() {
             <h5>In this lesson, we will learn the basics about the Python coding language!</h5>
             <img src="https://idsb.tmgrup.com.tr/ly/uploads/images/2022/08/22/226382.jpg" alt="Python logo"></img>
         </div>
-          <button><Link to="/modules">Try Sample Lesson</Link></button>
+          <Link to="/modules"><button>Try Sample Lesson</button></Link>
         </div>
 
       <div className='playGamesCard'>
@@ -27,7 +27,7 @@ export default function Home() {
           <h2>Play games and learn how to code at the same time!</h2>
           <h4>CodeQuest is designed to give kids a fun and interactive environment to learning. Give it a try!</h4>
         </div>
-        <button><Link to="/register">Sign Up</Link></button>
+        <Link to="/register"><button>Sign Up</button></Link>
         <button><Link to="/modules">Try Sample Lesson</Link></button>
       </div>
     </div>
