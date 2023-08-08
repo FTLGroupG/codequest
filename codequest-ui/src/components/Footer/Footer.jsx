2960b1523806f2085194ec9ec6fe5d5a7b0e79d3
AlejandroIrizarry <alejandroirizarry@mail.com>
2023-08-07 18:17:28 -0700

    Imported codequest logo

--- codequest-ui/src/components/Footer/Footer.jsx
+++ codequest-ui/src/components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
+import codequest_logo from "/src/assets/codequest-logo.png";
 
 export default function Footer() {
   return (
     <div className="Footer">
       <div className="content">
         <Link to="/">
-          <img src="/src/assets/codequest-logo.png" alt="logo" width="100px" />
+          <img src={codequest_logo} alt="logo" width="100px" />
         </Link>
         <h3>Copyright © 2023</h3>
         <h3>Privacy Policy</h3>
